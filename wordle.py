fccf5761537b57819078f5f7a0e5317ce665a933
bosouth <82606856+bosouth@users.noreply.github.com>
2022-03-15 11:22:45 -0500

    continue work on dictionary

--- wordle.py
+++ wordle.py
@@ -8,7 +8,14 @@ data = open("valid.txt", "r")
 # mark 1 if letter in word but not in right place
 # mark 2 if letter in word at the right place
 
+def isValidWord(guess, words):
+    isValid = False
+    if guess in words:
+        isValid = True
+    return isValid
+
 def compare(sol, guess):
+    # ensure that double letter guess are counted for
     i = 0
     solArray = []
     for ele in guess:
@@ -21,32 +28,50 @@ def compare(sol, guess):
         i += 1
     return solArray
 
-def newDictionary(tup, d):
+def newDictionary(tup, d, guess):
     newDict = []
+    tempDict = []
     for word in d:
         for l in range(len(word)):
             if tup[l][1] == 0:
-                break
-            # else:
-            #     if tup[l][0] in word:
-            #         newDict.append(word)
+                continue
             elif tup[l][1] == 1:
                 if tup[l][0] in word:
-                    newDict.append(word)
+                    tempDict.append(word)
+                break
             else:
                 if tup[l][0] == word[l]:
-                    newDict.append(word)
+                    tempDict.append(word)
+                break
+    if tempDict == []:
+        return d
 
-    w = 0
-    for l in range(len(newDict[w])):
-        w += 1
-        currentWord = newDict[w]
-        if tup[l][1] != 2:
-            newDict.remove(newDict[w])
-            w -= 1
-            continue
-        
+    # find indexes of each letter whose value is 2
+    # only return words that have ALL green words
+    green = []
+    yellow = []
+    for m in range(len(tup)):
+        if tup[m][1] == 2:
+            green.append((tup[m][0], m))
+        if tup[m][1] == 1:
+            yellow.append(tup[m][0])
+
+    if len(green) == 0:
+        return tempDict
+
+    for w in tempDict:
+        for ele in green:
             
+            if green.index(ele) == len(green) - 1:
+                if w[ele[1]] == ele[0]:
+                    newDict.append(w)
+                    break
+
+            if w[ele[1]] == ele[0]:
+                continue
+            
+            else:
+                break
     return newDict  
     # return None
 
@@ -55,23 +80,28 @@ words = data.readlines()
 words = words[0].split(",")
 words = [w.strip("\"") for w in words]
 
-goalWord = "lawns"
+goalWord = "beaux"
 attempt = 1
 
-print(goalWord)
 
 while attempt < 7:
     r = rand.randint(0, len(words) -1)
 
-    guess = "loans"
+    guess = "betas"
+
+    if isValidWord(guess, words) == False:
+        print("Invalid word")
+        break
 
     #compare
     tup = compare(goalWord, guess)
 
     # return dictionary with remaining valid words
-    new = newDictionary(tup, words)
+    new = newDictionary(tup, words, guess)
 
-    print(guess)
+    print(new)
+
+    # use knn to determine closest word, rinse and repeat
     
     attempt += 1
 
